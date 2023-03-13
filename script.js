7baf5597beb7f8ad6166e717735e79d3d85f2ef7
Parv Gupta <Parvg555@gmail.com>
2023-03-14 01:18:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+function changeColor(id) {
+			// Loop through all the squares
+			for (var i = 1; i <= 3; i++) {
+				// If the square is not the one being hovered over
+				if (i !== id) {
+					// Change its color
+					document.getElementById("square" + i).style.backgroundColor = "#6F4E37"; //coffee
+				}
+			}
+		}
+
+		function resetColor(id) {
+			// Loop through all the squares
+			for (var i = 1; i <= 3; i++) {
+				// If the square is not the one being hovered over
+				if (i !== id) {
+					// Reset its color
+					document.getElementById("square" + i).style.backgroundColor = "#E6E6FA";
+				}
+			}
+		}
